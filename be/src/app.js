beb8c7bfe7e176e0b85d55a2314cc8f54105a8cb
Nguyễn An <annguyen04@hotmail.com>
2024-04-20 14:14:13 +0700

    be

---
+++ be/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+const app = express();
+// const app = express.createServer();
+app.use(cors());
+
+app.use(express.json());
+mongoose.connect(process.env.CONNECTION_STRING_MONGODB).then(() => {
+  console.log("Connected!");
+});
+const port = process.env.PORT || 3000;
+
+import routerUser from "./routers/user.router.js";
+app.use("/user", routerUser);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ be/src/routers/user.router.js
@@ -0,0 +1,26 @@
+import {
+  getUsers,
+  getUserById,
+  register,
+  uploadUser,
+  login,
+  deleteUser,
+} from "../controllers/user.controller.js";
+import express from "express";
+
+const router = express.Router();
+
+// get all users
+router.get("/", getUsers);
+// get user by id
+router.get("/:id", getUserById);
+// register
+router.post("/register", register);
+// login
+router.post("/login", login);
+// upload
+router.put("/:id", uploadUser);
+// delete
+router.delete("/:id", deleteUser);
+
+export default router;
