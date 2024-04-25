4fcf2ba072ffc028fc0fac2df10559e19feeb2bb
ledo9124 <leminhdo904@gmail.com>
2024-04-26 02:21:19 +0700

    fix admin table

--- fe/src/components/ModalDetailTable.js
+++ fe/src/components/ModalDetailTable.js
@@ -4,7 +4,7 @@ import Table from "./Table";
 export const ModalDetailTable = ({table , handleMobile}) => {
     return (
         <dialog id={`${table._id}`} className="modal">
-            <div className="modal-box bg-white">
+            <div className="modal-box bg-white p-0">
                 <Table table={table} mobile={true} onMobile={handleMobile} />
             </div>
             <form method="dialog" className="modal-backdrop">
