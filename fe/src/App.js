257c83f507d4063e973499164088b9140583e05b
Nguyễn An <annguyen04@hotmail.com>
2024-04-28 14:05:02 +0700

    message

--- fe/src/App.js
+++ fe/src/App.js
@@ -14,6 +14,7 @@ import { jwtDecode } from "jwt-decode";
 import Home from "./pages/Home.js";
 import NotFound from "./pages/NotFound.js";
 import { getTables } from "./apis/table.api.ts";
+import Message from "./components/Message.js";
 function App() {
   const [listTables, setListTables] = useState([]);
   useEffect(() => {
@@ -37,6 +38,9 @@ function App() {
 
   return (
     <>
+      <Routes path={`/`}>
+        <Route path={`/message`} element={<Message />}></Route>
+      </Routes>
       <Header />
       <div className="container">
         <Routes>
